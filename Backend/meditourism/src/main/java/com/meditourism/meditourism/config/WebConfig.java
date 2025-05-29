27aa058dc92e68e6546123bb571d34ffb2f25ea8
Andres06272 <monroyrojasandresfelipe@gmail.com>
2025-05-29 06:21:48 -0500

    se añadio a cors

--- Backend/meditourism/src/main/java/com/meditourism/meditourism/config/WebConfig.java
+++ Backend/meditourism/src/main/java/com/meditourism/meditourism/config/WebConfig.java
@@ -13,7 +13,8 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins(
                         "http://localhost:10090", // Frontend local development
                         "http://127.0.0.1:5501", // Alternate local frontend
-                        "https://e74d-2803-1800-133a-e914-a34b-1e51-ff20-8ff4.ngrok-free.app" // Public ngrok URL
+                        "https://e74d-2803-1800-133a-e914-a34b-1e51-ff20-8ff4.ngrok-free.app", // Public ngrok URL
+                        "https://meditourism-zszy.vercel.app" // New Vercel frontend URL
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH") // Allow all necessary HTTP methods
                 .allowedHeaders("*") // Allow all headers
